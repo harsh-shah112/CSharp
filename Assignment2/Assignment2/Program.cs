d5b8c79d80e4f1670a733999bc9ac511630fd1a6
Harsh Shah <harsh@promact.com>
2018-02-10 13:17:39 +0530

    C# Assignment2

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            List<string> a = new List<string>()
+            {"Tomato",
+             "Potato",
+             "ToMato",
+             "Potato",
+             "Red Tomato",
+             "Tomato  Catchup",
+             "Grapes",
+             "grapes",
+             "potato" };
+            
+            int c = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i].Equals("tomato", StringComparison.OrdinalIgnoreCase))
+                {
+                    c++;
+                }
+                    
+             }
+            Console.WriteLine("count of tomato="+c);
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i].Equals("Potato"))
+                {
+                    Console.WriteLine("index of Potato="+i);
+                }
+            }
+        }
+    }
+}
---
+++ Assinment_part2/Assinment_part2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assinment_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float X = 0.06f;
+            float Y = 5.94f;
+            float sum = X + Y;
+            Console.WriteLine("Sum of X AND Y ="+" "+sum);
+
+            if(sum == 6)
+            { Console.WriteLine("the summation of X & Y are equal to 6"); }
+            else
+            { Console.WriteLine("the summation of X & Y are not equal to 6"); }
+
+            if(X>Y)
+            { Console.WriteLine("Greater Value="+X); }
+            else
+            { Console.WriteLine("Greater Value="+Y); }
+        }
+    }
+}
